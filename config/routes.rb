05050099ef85571642980f29da66492e6615caa6
Ravi-M <raviatdesk@gmail.com>
2012-08-23 15:30:16 +0530

    Layout done and routes added like signup button n all

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  #get "users/new"
+
   #get "static_pages/home"
   #get "static_pages/help"
   #get "static_pages/about"
@@ -6,10 +8,11 @@ SampleApp::Application.routes.draw do
   
   root to: 'static_pages#home'
 
-  #match '/',    to: 'static_pages#home'
+  match '/home',    to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup',     to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
